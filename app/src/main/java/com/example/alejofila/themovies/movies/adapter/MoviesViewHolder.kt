e092be6fc1806d20ab6e82e06303f8eccde0c3e8
Alejandro Agudelo Vergara <alejofila@gmail.com>
2019-03-04 23:51:19 +0100

    Replacing Picasso with glide to improve performance in image loading.

--- app/src/main/java/com/example/alejofila/themovies/movies/adapter/MoviesViewHolder.kt
+++ app/src/main/java/com/example/alejofila/themovies/movies/adapter/MoviesViewHolder.kt
@@ -1,55 +1,46 @@
 package com.example.alejofila.themovies.movies.adapter
 
 import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.support.v7.graphics.Palette
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.BitmapImageViewTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.alejofila.data.network.TmdbService
 import com.example.alejofila.themovies.common.uimodel.MoviesUiModel
-import com.squareup.picasso.Picasso
-import com.squareup.picasso.Target
 import kotlinx.android.synthetic.main.movie_item.view.*
+
 
 class MoviesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun bind(movies: MoviesUiModel) {
-        itemView.run {
-            item_poster_title.text = movies.title
-            item_poster_overview.text = movies.overview
-            item_poster_release_date.text = movies.releaseDate
-            movies.imagePath?.let {
-                Picasso.with(this.context)
-                    .load(TmdbService.getPosterPath(movies.imagePath))
-                    .resize(300,300)
-                    .noFade()
-                    .into(object : Target {
-                        override fun onPrepareLoad(placeHolderDrawable: Drawable?) {
-                        }
-
-                        override fun onBitmapFailed(errorDrawable: Drawable?) {
-                        }
-
-                        override fun onBitmapLoaded(bitmap: Bitmap, from: Picasso.LoadedFrom?) {
-                            item_poster_post.setImageBitmap(bitmap)
-                            Palette.from(bitmap)
-                                .generate { palette ->
-                                    val textWatch = palette?.vibrantSwatch
-                                    textWatch?.let {
-                                        item_poster_palette.setBackgroundColor(textWatch.rgb)
-                                        item_poster_release_date.setTextColor(textWatch.bodyTextColor)
-                                        item_poster_title.setTextColor(textWatch.titleTextColor)
-                                    }
-
-                                }
-                        }
-                    })
-            }
-
-
+        itemView.item_poster_title.text = movies.title
+        itemView.item_poster_overview.text = movies.overview
+        itemView.item_poster_release_date.text = movies.releaseDate
+        if (movies.imagePath == null) {
+            return
         }
+        Glide.with(itemView)
+            .asBitmap()
+            .load(TmdbService.getPosterPath(movies.imagePath))
+            .into(object : BitmapImageViewTarget(itemView.item_poster_post) {
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    super.onResourceReady(resource, transition)
+                    Palette.PaletteAsyncListener { palette ->
+                        val textWatch = palette?.vibrantSwatch ?: return@PaletteAsyncListener
+                        textWatch.let {
+                            itemView.item_poster_palette.setBackgroundColor(textWatch.rgb)
+                            itemView.item_poster_release_date.setTextColor(textWatch.bodyTextColor)
+                            itemView.item_poster_title.setTextColor(textWatch.titleTextColor)
+
+                        }
+                    }
+                }
+            })
     }
-    fun clear(){
+
+
+    fun clear() {
         itemView.item_poster_post.setImageDrawable(null)
     }
-
 }

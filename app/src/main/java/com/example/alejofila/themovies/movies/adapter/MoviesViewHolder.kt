968d1a1498506fb3d67d59eae4202f5fc4409b40
Alejandro Agudelo Vergara <alejofila@gmail.com>
2019-03-05 00:51:54 +0100

    Refactoring MoviesViewHolder, to reduce cyclomatic complexity

--- app/src/main/java/com/example/alejofila/themovies/movies/adapter/MoviesViewHolder.kt
+++ app/src/main/java/com/example/alejofila/themovies/movies/adapter/MoviesViewHolder.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.support.v7.graphics.Palette
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.BitmapImageViewTarget
 import com.bumptech.glide.request.transition.Transition
@@ -14,33 +15,38 @@ import kotlinx.android.synthetic.main.movie_item.view.*
 
 class MoviesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     fun bind(movies: MoviesUiModel) {
-        itemView.item_poster_title.text = movies.title
-        itemView.item_poster_overview.text = movies.overview
-        itemView.item_poster_release_date.text = movies.releaseDate
-        if (movies.imagePath == null) {
-            return
+        itemView.run {
+            item_poster_title.text = movies.title
+            item_poster_overview.text = movies.overview
+            item_poster_release_date.text = movies.releaseDate
+            if (movies.imagePath.isNullOrBlank()) {
+                return
+            }
+            Glide.with(this)
+                .asBitmap()
+                .load(TmdbService.getPosterPath(movies.imagePath))
+                .into(CustomBitMapImageViewTarget(this, item_poster_post))
         }
-        Glide.with(itemView)
-            .asBitmap()
-            .load(TmdbService.getPosterPath(movies.imagePath))
-            .into(object : BitmapImageViewTarget(itemView.item_poster_post) {
-                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                    super.onResourceReady(resource, transition)
-                    Palette.PaletteAsyncListener { palette ->
-                        val textWatch = palette?.vibrantSwatch ?: return@PaletteAsyncListener
-                        textWatch.let {
-                            itemView.item_poster_palette.setBackgroundColor(textWatch.rgb)
-                            itemView.item_poster_release_date.setTextColor(textWatch.bodyTextColor)
-                            itemView.item_poster_title.setTextColor(textWatch.titleTextColor)
-
-                        }
-                    }
-                }
-            })
     }
-
-
     fun clear() {
         itemView.item_poster_post.setImageDrawable(null)
+    }
+
+}
+
+class CustomBitMapImageViewTarget(private val itemView: View, imageView: ImageView) : BitmapImageViewTarget(imageView) {
+    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+        super.onResourceReady(resource, transition)
+        Palette.PaletteAsyncListener { palette ->
+            val textWatch = palette?.vibrantSwatch ?: return@PaletteAsyncListener
+            textWatch.let {
+                itemView.run {
+                    item_poster_palette.setBackgroundColor(textWatch.rgb)
+                    item_poster_release_date.setTextColor(textWatch.bodyTextColor)
+                    item_poster_title.setTextColor(textWatch.titleTextColor)
+                }
+
+            }
+        }
     }
 }
